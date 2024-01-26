fc50bc1eb4cc2c9302a1736348015d5e0699f92e
LucaBrunori <brunori.luca21@gmail.com>
2024-01-26 14:07:36 +0100

    Prima modifica

--- src/gestionefile/GestioneFile.java
+++ src/gestionefile/GestioneFile.java
@@ -1,5 +1,6 @@
 package gestionefile;
 
+import java.util.Scanner;
 /**
  *
  * @author Luca Brunori
@@ -11,7 +12,6 @@ public class GestioneFile {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
     
         //1)LETTURA
         Lettore lettore = new Lettore("user.json");
@@ -19,11 +19,30 @@ public class GestioneFile {
         
         //2)ELABORAZIONE
         //INSERIRE US PWD
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Inserisci l'username: ");
+        String username = scanner.nextLine();
+        
+        System.out.print("Inserisci la password: ");
+        String password = scanner.nextLine();
+        
+        if (checkLogin(username, password)) {
+            System.out.println("Accesso consentito. Benvenuto, " + username + "!");
+        } else {
+            System.out.println("Accesso negato. Controlla l'username e la password e riprova.");
+        }
+        
+        scanner.close();
         //3) SCRITTURA
         Scrittore scrittore = new Scrittore("output.csv");
         Thread threadScrittore = new Thread(scrittore);
         threadScrittore.start();
+        //FileInputStream inputStream = new FileInputStream(inFile);
+    }
+
+    private static boolean checkLogin(String username, String password) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
 }
